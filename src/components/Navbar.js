f9a43b5b3ef861eacaaa25966e13b9accc2f1b60
korykkb <korysb@hawaii.edu>
2024-07-23 12:37:05 -1000

    Add Navbar to travel journal

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar () {
+    return (
+        <nav className="nav">
+            <i className="fa-solid fa-earth-americas"></i>
+            <h2 className="nav-title">My Travel Journal</h2>
+        </nav>
+    )
+}
